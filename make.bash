e09ad954e3555c3d5a4fdb8e1c73409505c9498f
Corey Minyard <cminyard@mvista.com>
2022-03-30 11:51:04 -0500

    Add a gensio patch for handling SRT and recovery properly
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- make.bash
+++ make.bash
@@ -49,7 +49,7 @@ GENSIOVERSION="2.4.0-rc5"
 GENSIODIST="gensio-${GENSIOVERSION}"
 GENSIODIST_BASEURL="https://sourceforge.net/projects/ser2net/files/ser2net"
 GENSIODIST_URL="${GENSIODIST_BASEURL}/${GENSIODIST}.tar.gz"
-GENSIODIST_PATCHES=""
+GENSIODIST_PATCHES="patches/0001-ax25-Clear-the-retry-count-when-a-response-is-heard.patch"
 function install_gensio {
 	mkdir -p .build && cd .build
 	if [ ! -f "${GENSIODIST}" ]; then
