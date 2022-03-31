318e5924c29ed23ab22c2963544208f677ecf615
Corey Minyard <cminyard@mvista.com>
2022-03-30 19:38:44 -0500

    Add a gensio patch to fix some ack issues
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- make.bash
+++ make.bash
@@ -49,7 +49,10 @@ GENSIOVERSION="2.4.0-rc5"
 GENSIODIST="gensio-${GENSIOVERSION}"
 GENSIODIST_BASEURL="https://sourceforge.net/projects/ser2net/files/ser2net"
 GENSIODIST_URL="${GENSIODIST_BASEURL}/${GENSIODIST}.tar.gz"
-GENSIODIST_PATCHES="patches/0001-ax25-Clear-the-retry-count-when-a-response-is-heard.patch"
+GENSIODIST_PATCHES="\
+	patches/0001-ax25-Clear-the-retry-count-when-a-response-is-heard.patch
+	patches/0001-ax25-Fix-some-ack-handling.patch \
+"
 function install_gensio {
 	mkdir -p .build && cd .build
 	if [ ! -f "${GENSIODIST}" ]; then
