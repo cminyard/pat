b16b64efc1e13823b14f577d6bd747cbd239f1ab
Corey Minyard <cminyard@mvista.com>
2022-04-18 14:59:30 -0500

    Move to gensio 2.4.0-rc7
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- make.bash
+++ make.bash
@@ -45,7 +45,7 @@ if [[ "$OS" == "linux"* ]]; then
 	fi
 fi
 
-GENSIOVERSION="2.4.0-rc6"
+GENSIOVERSION="2.4.0-rc7"
 GENSIODIST="gensio-${GENSIOVERSION}"
 GENSIODIST_BASEURL="https://sourceforge.net/projects/ser2net/files/ser2net"
 GENSIODIST_URL="${GENSIODIST_BASEURL}/${GENSIODIST}.tar.gz"
