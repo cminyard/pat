4047a15e810bee24abbc074f013c24306b62791b
Corey Minyard <cminyard@mvista.com>
2022-08-10 20:48:23 -0500

    Move to gensio 2.5.2
    Add script support.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- make.bash
+++ make.bash
@@ -55,7 +55,7 @@ if [[ "$OS" == "linux"* ]]; then
 	fi
 fi
 
-GENSIOVERSION="2.5.1"
+GENSIOVERSION="2.5.2"
 GENSIODIST="gensio-${GENSIOVERSION}"
 GENSIODIST_BASEURL="https://sourceforge.net/projects/ser2net/files/ser2net"
 GENSIODIST_URL="${GENSIODIST_BASEURL}/${GENSIODIST}.tar.gz"
