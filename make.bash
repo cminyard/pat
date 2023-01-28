ce76d766e769fa9b77f286d49f83c17918e75713
Corey Minyard <cminyard@mvista.com>
2023-01-28 08:47:45 -0600

    Move to gensio version 2.6.2
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- make.bash
+++ make.bash
@@ -56,7 +56,7 @@ if [[ "$OS" == "linux"* ]]; then
 	fi
 fi
 
-GENSIOVERSION="2.6.0-rc6"
+GENSIOVERSION="2.6.2"
 GENSIODIST="gensio-${GENSIOVERSION}"
 GENSIODIST_BASEURL="https://sourceforge.net/projects/ser2net/files/ser2net"
 GENSIODIST_URL="${GENSIODIST_BASEURL}/${GENSIODIST}.tar.gz"
