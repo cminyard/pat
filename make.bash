5b6a0c5ee984822e516bc8d59dd461d3cae1ab9b
Corey Minyard <minyard@acm.org>
2023-11-23 18:34:25 -0600

    Move to gensio version 2.8.0
    Signed-off-by: Corey Minyard <minyard@acm.org>

--- make.bash
+++ make.bash
@@ -63,7 +63,7 @@ else
 	fi
 fi
 
-GENSIOVERSION="2.7.4"
+GENSIOVERSION="2.8.0"
 GENSIODIST="gensio-${GENSIOVERSION}"
 GENSIODIST_BASEURL="https://sourceforge.net/projects/ser2net/files/ser2net"
 GENSIODIST_URL="${GENSIODIST_BASEURL}/${GENSIODIST}.tar.gz"
